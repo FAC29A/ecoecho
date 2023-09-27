74afcdd79fa5c5c0554d4874b9e9e90c967f5f87
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-09-27 01:54:43 +0100

    feat: add scroll top button functionality and update button position

--- script.js
+++ script.js
@@ -15,3 +15,41 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+const sections = document.querySelectorAll('section');
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
+
+
+window.addEventListener('scroll', () => {
+  let closestSection = null;
+  let minDistance = Infinity;
+
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    const distance = Math.abs(rect.bottom);
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      closestSection = section;
+    }
+  });
+
+  if (closestSection) {
+    const rect = closestSection.getBoundingClientRect();
+    const btnHeight = scrollTopBtn.offsetHeight;
+
+    // Update the button's position.
+    scrollTopBtn.style.bottom = (rect.bottom - btnHeight - 10) + 'px';
+  }
+
+  // Showing/hiding the button based on scroll position.
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = 'block';
+  } else {
+    scrollTopBtn.style.display = 'none';
+  }
+});
