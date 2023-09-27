a58d56b00cfa5ae8ee2652e645fa851138e6bb5d
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-09-28 00:10:35 +0100

    I added media queries and hamburger menu nav for mobile phone. Add comments in CSS and JS.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get the modal, teaser button, and close button elements from the DOM
 var modal = document.getElementById("videoModal");
 var btn = document.getElementById("teaserButton");
 var span = document.getElementsByClassName("close")[0];
@@ -18,9 +19,11 @@ window.onclick = function (event) {
   }
 };
 
+// When the document is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   let scrollToTopButton = document.getElementById("scrollTopBtn");
 
+   // When the user scrolls the page
   window.addEventListener("scroll", function () {
     if (
       document.body.scrollTop > 100 ||
@@ -33,7 +36,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  // When the user clicks the scroll to top button, smoothly scroll them to the top of the page
   scrollToTopButton.addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 });
+
+
+// Toggle the mobile menu visibility when the hamburger button is clicked
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+  const nav = document.getElementById('nav-menu');
+  nav.classList.toggle('mobile-active');
+});
+
